8d057268ca43a0dd541006092de916e11f50ec58
rahmatheruka <rahmatheruka@gmail.com>
2014-10-14 19:47:59 +0700

    first commit

---
+++ application/views/dashboard/include/footer.php
@@ -0,0 +1,6 @@
+<script src="<?php echo base_url('assets/libraries/jquery.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/js/libraries/bootstrap/js/bootstrap.min.js'); ?>"></script>
+<script src="<?php echo base_url('assets/custom/js/main.js'); ?>"></script>
+
+</body>
+</html>
---
+++ application/views/dashboard/include/head.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Dashboard</title>
+	
+	<link rel="stylesheet" href="<?php echo base_url('assets/libraries/bootstrap/css/bootstrap.min.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/libraries/bootstrap/css/bootstrap-theme.min.css'); ?>">
+	<link rel="stylesheet" href="<?php echo base_url('assets/custom/css/main.css'); ?>">
+</head>
+
+<body>
